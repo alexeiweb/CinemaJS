a252f71933d39d7570849d8fd6c2349713d1dd60
Alexei Web <alexeiiweb@gmail.com>
2021-12-09 12:43:51 +0300

    day2

---
+++ js/services.js
@@ -0,0 +1,19 @@
+const API_KEY = '79a31251f5f35d0ca496f4fe2cc33e43';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+// trending/all/day?api_key=<<api_key>>
+
+const getData = url => fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+    throw `Что-то пошло не так, ошибка ${response.status}`
+  })  
+  .catch(err => console.error(err));
+
+export const getTrends = async (type = 'all', period = 'day', page = 1) => {
+  const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+  return await getData(url);
+}
