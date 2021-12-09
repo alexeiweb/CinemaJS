7ef94beca29d5dfbcea2952514db5da2fb541217
Alexei Web <alexeiiweb@gmail.com>
2021-12-09 16:21:42 +0300

    add cards

--- js/services.js
+++ js/services.js
@@ -2,8 +2,6 @@ const API_KEY = '79a31251f5f35d0ca496f4fe2cc33e43';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const LANGUAGE = '&language=ru-RU';
 
-// trending/all/day?api_key=<<api_key>>
-
 const getData = url => fetch(url)
   .then(response => {
     if (response.ok) {
@@ -15,5 +13,5 @@ const getData = url => fetch(url)
 
 export const getTrends = async (type = 'all', period = 'day', page = 1) => {
   const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
-  return await getData(url);
+  return getData(url);
 }
