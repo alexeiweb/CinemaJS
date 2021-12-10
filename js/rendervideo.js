1cabd8e4f516a8fef0ed28023b4521f8c1b25c3f
Alexei Web <alexeiiweb@gmail.com>
2021-12-10 14:14:36 +0300

    video not working

--- js/rendervideo.js
+++ js/rendervideo.js
@@ -1,27 +1,43 @@
-import { getTrends } from "./services.js";
+import { getTrends, getVideo } from "./services.js";
 import renderCard from "./renderCard.js";
 
 const filmWeek = document.querySelector('.film-week');
 
-const firstRender = data => {
+const firstRender = (data, { key }) => {
+  const {
+    vote_average,
+    backdrop_path,
+    name,
+    original_name,
+    title,
+    original_title
+  } = data
 
   filmWeek.innerHTML = `
-    <div class="container film-week__container" data-rating="${data.vote_average}">
+    <div class="container film-week__container" data-rating="${vote_average}">
       <div class="film-week__poster-wrapper">
-        <img class="film-week__poster" src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${data.backdrop_path}" alt="постер ${data.name}">
-        <p class="film-week__title_origin">${data.original_name || data.original_title}</p>
+        <img class="film-week__poster" src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${backdrop_path}" alt="постер ${name || title}">
+        <p class="film-week__title_origin">${original_name || original_title}</p>
       </div>
-      <h2 class="film-week__title">${data.name || data.title}</h2>
-      <a class="film-week__watch-trailer tube" href="https://youtu.be/V0hagz_8L3M" aria-label="смотреть трейлер"></a>
+      <h2 class="film-week__title">${name || title}</h2>
+      ${key ? 
+        `<a class="film-week__watch-trailer tube" 
+        href="https://youtu.be/${key}" 
+        aria-label="смотреть трейлер"></a>` : 
+        ''}      
     </div>
             `;
 }
 
 const renderVideo = async () => {
   const data = await getTrends();
+
   const [ firstCard, ...otherCard ] = data.results;
   otherCard.length = 16;
-  firstRender(firstCard);
+
+  const video = await getVideo(firstCard.id, firstCard.media_type)
+
+  firstRender(firstCard, video.results[0]);
   renderCard(otherCard);
 };
 
